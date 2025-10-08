964cd94a2a7333d3f9e712b4eb32841591300c82
Toren2313 <przemo_653@poczta.fm>
2025-10-08 18:24:15 +0200

    perf: :zap: Improved user dto
    Used PickType & OmitType instead of repeat this same fields

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,49 +1,6 @@
 import { Expose, Exclude } from "class-transformer";
 import { IsDate, IsEmail, IsNotEmpty, IsNumber, IsString, MaxLength, MinLength, ValidateIf } from "class-validator";
-
-export class CreateUserDTO {
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(16)
-  username: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  @MaxLength(32)
-  password: string;
-}
-
-export class LoginUserDTO {
-  @ValidateIf((obj) => !obj.username)
-  @IsEmail()
-  email: string;
-
-  @ValidateIf((obj) => !obj.email)
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(16)
-  username: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  @MaxLength(32)
-  password: string;
-}
-
-export class ChangePasswordUserDTO {
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  @MaxLength(32)
-  password: string;
-}
+import { OmitType, PickType } from "@nestjs/swagger";
 
 export class UserDTO {
   @Expose()
@@ -51,6 +8,8 @@ export class UserDTO {
   id: number;
 
   @Expose()
+  @MinLength(3)
+  @MaxLength(16)
   @IsString()
   @IsNotEmpty()
   username: string;
@@ -61,6 +20,8 @@ export class UserDTO {
   email: string;
 
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
   @Exclude({ toPlainOnly: true })
   @IsString()
   password: string;
@@ -72,4 +33,13 @@ export class UserDTO {
   @Expose()
   @IsDate()
   updatedAt: Date;
+}
+
+export class ChangePasswordUserDTO extends PickType(UserDTO, ["password"] as const) {}
+export class CreateUserDTO extends OmitType(UserDTO, ["id", "createdAt", "updatedAt"]) {}
+export class LoginUserDTO extends OmitType(UserDTO, ["id", "createdAt", "updatedAt"]) {
+  @ValidateIf((obj) => !obj.username)
+  email: string;
+  @ValidateIf((obj) => !obj.email)
+  username: string;
 }
