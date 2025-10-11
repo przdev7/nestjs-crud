fafae0507a653bd5e281c09aaef13e8e955db6c5
Toren2313 <przemo_653@poczta.fm>
2025-10-11 23:29:11 +0200

    feat: :sparkles: small feature only for admin
    Created route find all users only for admin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Request, Get } from "@nestjs/common";
 import { Roles } from "../common/decorators/auth.decorator";
+import { roles } from "../shared";
+import { UsersService } from "./users.service";
 
 @Controller("users")
 export class UsersController {
+  constructor(private readonly user: UsersService) {}
+
   @Get("me")
   @Roles([])
   async me(@Request() req) {
     return req.user;
+  }
+
+  @Get("all")
+  @Roles([roles.ADMIN])
+  async findAll() {
+    return await this.user.findAll();
   }
 }
